899cdf9ce89e9e66e9a13eb2140f1536ca1014c1
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2024-04-08 14:07:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { ItemListContainer } from './ItemListContainer/ItemListContainer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<ItemListContainer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ImgItemList from './ImgItemList'
+
+export const ItemListContainer = () => {
+return (<>
+    <ImgItemList />
+    <div>ItemListContainer</div>
+    
+</>)
+}
