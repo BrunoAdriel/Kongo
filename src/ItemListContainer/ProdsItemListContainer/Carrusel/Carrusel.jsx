9f2abb298068758d578900b09a296fc6d8d17627
BrunoAdriel <109391716+BrunoAdriel@users.noreply.github.com>
2024-04-09 18:41:41 -0300

    agrego todo las caracteristicas de la imagen

--- src/ItemListContainer/ProdsItemListContainer/Carrusel/Carrusel.jsx
+++ src/ItemListContainer/ProdsItemListContainer/Carrusel/Carrusel.jsx
@@ -4,28 +4,44 @@ const Carrusel = ({ imagenes }) => {
     const [visibleStartIndex, setVisibleStartIndex] = useState(0);
 
     const next = () => {
-    setVisibleStartIndex((prevIndex) => (prevIndex + 1) % imagenes.length);
-};
+        setVisibleStartIndex((prevIndex) => (prevIndex + 1) % imagenes.length);
+    };
 
     const prev = () => {
-    setVisibleStartIndex((prevIndex) => (prevIndex - 1 + imagenes.length) % imagenes.length);
-};
+        setVisibleStartIndex((prevIndex) => (prevIndex - 1 + imagenes.length) % imagenes.length);
+    };
 
-return (
-        <div id="miCarrusel" className="carousel slide" data-bs-ride="carousel" data-bs-interval="false">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <div className="row">
-                        {[0, 1, 2, 3].map((offset) => {
-                        const imageIndex = (visibleStartIndex + offset) % imagenes.length;
-                        return (
-                            <div className="col" key={imageIndex}>
-                            <img src={imagenes[imageIndex].src} className="d-block w-100" alt={imagenes[imageIndex].alt} />
+
+
+    return (
+        <div className="carrusel-container">
+            <div className="carrusel-wrapper">
+                {[0, 1, 2, 3].map((offset) => {
+                    const imageIndex = (visibleStartIndex + offset) % imagenes.length;
+                    
+                    const CalcularPrecio = (precio) =>{
+                        return (precio / 6).toFixed(2)
+                    }                
+                    const precioProd = imagenes[imageIndex].price
+                    const precioDividio = CalcularPrecio(precioProd)
+
+                    return (
+                        <div className="carrusel-item" key={imageIndex}>
+                            <img src={imagenes[imageIndex].src} alt={imagenes[imageIndex].alt} />
+                            <div className="carrusel-caption">
+                                <p>{imagenes[imageIndex].name}</p>
+                                <p>${imagenes[imageIndex].price}</p>
+                                <section className='cuotasDeDescuento'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-credit-card-2-front" viewBox="0 0 16 16">
+                                    <path d="M14 3a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1zM2 2a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2z"/>
+                                    <path d="M2 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m0 2a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5m3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5m3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5m3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 0 1h-1a.5.5 0 0 1-.5-.5"/>
+                                    </svg>
+                                    <p>Hasta 6 coutas sin interes de $: {precioDividio}</p>
+                                </section>
                             </div>
-                        );
-                        })}
-                    </div>
-                </div>
+                        </div>
+                    );
+                })}
             </div>
             <button className="carousel-control-prev" type="button" onClick={prev}>
                 <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -34,7 +50,7 @@ return (
                 <span className="carousel-control-next-icon" aria-hidden="true"></span>
             </button>
         </div>
-);
+    );
 };
 
 export default Carrusel;
